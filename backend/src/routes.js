982761e24f552a91486c99dd37860058c4dda994
GilsonSantos <gilsonjosert@gmail.com>
2020-01-17 01:00:30 -0400

    Search user github and create Dev mongoDB.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,44 @@
 const { Router } = require('express')
+const axios = require('axios')
+const Dev = require('./models/Dev')
 
 const routes = Router()
 
-routes.post('/users',(request,response)=>{
-    console.log(request.query)
-    return response.json({mensage:'teste'})
+routes.post('/devs', async (request,response)=>{
+    const { github_username, techs } = request.body
+
+    const user = await axios.get(`https://api.github.com/users/${github_username}`)
+
+    const techsArray = techs.split(',').map(tech=> tech.trim())
+
+    const { name = login, avatar_url, bio } = user.data
+
+    const dev = await Dev.create({
+      name,
+      github_username,
+      avatar_url,
+      bio,
+      techs:techsArray
+    });
+
+    return response.json(dev);
+})
+
+routes.get('/devs/:id', (request, response) => {
+    console.log(request.params)
+    return response.json({
+        ...request.params
+    })
+})
+
+routes.get('/devs', (request, response) => {
+    return response.json({
+        ...request.params
+    })
+})
+
+routes.get('/',(request,response)=>{
+    return response.json({mensage:'Gilson Santos'})
 })
 
 module.exports = routes
