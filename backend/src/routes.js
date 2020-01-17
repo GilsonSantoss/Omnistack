b69979fe43c87bfc7e0ffe515a899d98447ada70
Gilson Santos <gilsonjosert@gmail.com>
2020-01-17 17:38:24 +0000

    Location add

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,45 +1,10 @@
 const { Router } = require('express')
-const axios = require('axios')
-const Dev = require('./models/Dev')
+const DevController = require('./controllers/DevController')
 
 const routes = Router()
 
-routes.post('/devs', async (request,response)=>{
-    const { github_username, techs } = request.body
-
-    const user = await axios.get(`https://api.github.com/users/${github_username}`)
-
-    const techsArray = techs.split(',').map(tech=> tech.trim())
-
-    const { name = login, avatar_url, bio } = user.data
-
-    const dev = await Dev.create({
-      name,
-      github_username,
-      avatar_url,
-      bio,
-      techs:techsArray
-    });
-
-    return response.json(dev);
-})
-
-routes.get('/devs/:id', (request, response) => {
-    console.log(request.params)
-    return response.json({
-        ...request.params
-    })
-})
-
-routes.get('/devs', (request, response) => {
-    return response.json({
-        ...request.params
-    })
-})
-
-routes.get('/',(request,response)=>{
-    return response.json({mensage:'Gilson Santos'})
-})
+routes.get('/devs', DevController.index)
+routes.post('/devs', DevController.store)
 
 module.exports = routes
 
@@ -52,3 +17,21 @@ module.exports = routes
 // Body: requst.body ( Dados para cracao ou alteracao de um registro )
 
 // MongoDB ( Nao-relacional )
+
+
+// routes.get('/devs/:id', (request, response) => {
+//     console.log(request.params)
+//     return response.json({
+//         ...request.params
+//     })
+// })
+
+// routes.get('/devs', (request, response) => {
+//     return response.json({
+//         ...request.params
+//     })
+// })
+
+// routes.get('/',(request,response)=>{
+//     return response.json({mensage:'Gilson Santos'})
+// })
