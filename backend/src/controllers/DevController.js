08603ac1e11988cc15d8f1b5fd95d1d574e16c46
GilsonSantos <gilsonjosert@gmail.com>
2020-01-18 00:09:09 -0400

    Back end finsh

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
 const Dev = require('../models/Dev')
+const parseStringAsArray = require('../utils/parseStringAsArray')
 
 // index, show, store, update, destroy
 
@@ -20,7 +21,7 @@ module.exports = {
 
         const { name = login, avatar_url, bio } = user.data
 
-        const techsArray = techs.split(',').map(tech=> tech.trim())
+        const techsArray = parseStringAsArray(techs)
 
         const location = {
             type:'Point',
@@ -38,4 +39,16 @@ module.exports = {
     }
 
     return response.json(dev)
-}}
+
+    },
+    async destroy(request, response) {
+
+    const { github_username } = request.body
+
+    let dev = await Dev.deleteOne({github_username})
+
+    return response.json(dev)
+    
+    }
+
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -4,10 +4,13 @@ const routes = require('./routes')
 
 const app = express()
 const port =  3000
+const stringConnection = 'mongodb+srv://gilsonsantosxd:g12345@cluster0-svgma.mongodb.net/gilsu?retryWrites=true&w=majority'
 
-mongoose.connect('mongodb+srv://gilsonsantosxd:g12345@cluster0-svgma.mongodb.net/gilsu?retryWrites=true&w=majority',{
+mongoose.set('useCreateIndex', true)
+
+mongoose.connect(stringConnection,{
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 })
 
 // Registro no express dependencias e configs
